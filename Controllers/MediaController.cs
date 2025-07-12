887be4133efa75178a6c5b52e2245f8a0a4b894c
Fajar Setia Pambudi <fajarsetiapambudi@gmail.com>
2025-07-12 23:51:09 +0700

    add file api

---
+++ Controllers/MediaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Perkebunan.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MediaController : ControllerBase
+{
+    [HttpGet]
+    [Route("{*filename}")]
+    public async Task<IActionResult> GetFile(string filename)
+    {
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", filename);
+        
+        if (!System.IO.File.Exists(filePath))
+        {
+            return NotFound($"File '{filename}' not found.");
+        }
+        var fileBytes = await System.IO.File.ReadAllBytesAsync(filePath);
+        return File(fileBytes, "application/octet-stream", filename);
+    }
+}
